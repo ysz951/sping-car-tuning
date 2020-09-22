420e450fc82aaecc4bd5304ed466308d179aca16
ysz951 <shengyang_zhou@baylor.edu>
2020-09-22 14:22:51 -0500

    add ch03

--- ch02/cars/src/main/java/cars/Gear.java
+++ ch02/cars/src/main/java/cars/Gear.java
@@ -10,10 +10,8 @@ public class Gear {
   private final String id;
   private final String name;
   private final Type type;
-//  Audio, Interior, Engine, Suspension, Tires
   public static enum Type {
     Audio, Interior, Engine, Suspension, Tires
-//    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
   }
 
 }
